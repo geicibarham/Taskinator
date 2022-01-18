03cd769dba49a82562b2e45a1fc442efa68a3dbc
geicibarham <geicianecosta43@gmail.com>
2022-01-18 13:52:48 -0500

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) { 
   event.preventDefault(); 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+
 
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
